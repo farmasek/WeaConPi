1e8d3ae60e6413759ecd2b2421134cef40fd22a0
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-02-21 23:22:19 +0100

    basic git fetch

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ## NodeJS Express server with Typescript
 
 This project is setup for future QraphQL project.
+
+### Road map / TODO list
+
+- [x] Create Node + express + Typescript setup
+- [x] Schedule, api fetch PoC
+- [ ] Add Editor config
+- [ ] Add ts linter ?
+- [ ] Mongoose PoC
+- [ ] GraphQL PoC
--- index.ts
+++ index.ts
@@ -2,13 +2,32 @@
  * Created by Farmas on 21.02.2017.
  */
 import * as express from 'express';
+import * as schedule from 'node-schedule';
+import * as req from 'request';
+
+
 const app = express();
 const PORT = 1000;
 
-app.get('/hello-world', (request, response) => {
-    response.send('Hello from hello world endpoint');
+const getUsersGithubInfo = (user: string, callback) => {
+    req.get(`https://api.github.com/users/${user}`,
+        {headers: {'User-Agent': 'Awesome-Octocat-App'}},
+        (error, res, body) => {
+            callback(body)
+        });
+}
+
+app.get('/find-me-on-github/:user', (request, response) => {
+    getUsersGithubInfo(request.params.user, (body) => response.send(`
+<pre>${JSON.stringify(JSON.parse(body), null, 2)}</pre>
+`));
+
 });
 
+
 app.listen(PORT, () => {
+    const scheduled = schedule.scheduleJob('1 * * * * *', () =>
+        console.log('hello from scheduledJob')
+    );
     console.log(`Start is up and running on localhost:${PORT}`)
 });
