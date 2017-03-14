73526117dbf51f43f1dcd4b3a9a594e157050ae2
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-03-14 08:09:21 +0100

    may data

--- neural/index.ts
+++ neural/index.ts
@@ -1,6 +1,6 @@
 /// <reference path="../node_modules/@types/node/index.d.ts" />
 import { Architect, Trainer, Network } from "synaptic";
-import { trainingSetApril } from "./learning-data/test-trainingset";
+import { mayTrainingSet } from "./learning-data/test-trainingset";
 import NeuralPersist from "./neuralPersistance";
 import LSTM = Architect.LSTM;
 
@@ -25,7 +25,7 @@ export const calculateExampleLSTM = () => {
   }
   let trainer = new Trainer(weaconPiBrain)
 
-  trainer.train(trainingSetApril, {
+  trainer.train(mayTrainingSet, {
     iterations: 2000,
     rate: 0.2,
     error: .0005,
@@ -33,6 +33,16 @@ export const calculateExampleLSTM = () => {
   });
 
   new NeuralPersist().persistFile(weaconPiBrain);
-  console.log(weaconPiBrain.activate([0.01, 0.03, 0.2016, 0, 0.099, 0.12]));
+  console.log('2/3/2016 - 7h, should be tmb w[0] - 0 | b [1] - 0 | heat [2] - 90')
+  console.log(weaconPiBrain.activate([0.02, 0.03, 0.2016, 0.07, 0.102, 0.23]));
+
+  console.log('11/5/2016 - 7h, should be tmb w[0] - 0 | b [1] - 1 | heat [2] - 10')
+  console.log(weaconPiBrain.activate( [0.11, 0.05, 0.2016, 0.11, 0.123, 0.23]));
+
+  console.log('14/4/2016 - 13h, should be tmb w[0] - 0 | b [1] - 1 | heat [2] - 80')
+  console.log(weaconPiBrain.activate([0.14, 0.04, 0.2016, 0.13, 0.112, 0.23]));
+
+  console.log('14/5/2016 - 12h, should be tmb w[0] - 1 | b [1] - 1 | heat [2] - 60')
+  console.log(weaconPiBrain.activate([0.14, 0.05, 0.2016, 0.12, 0.119, 0.23]));
 }
 
